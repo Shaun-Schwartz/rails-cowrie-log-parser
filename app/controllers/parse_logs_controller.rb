f479dd701c4f77dc8c920487b4faa81444bdca94
Shaun Schwartz <me@shaunschwartz.com>
2018-01-09 13:15:09 -0800

    change geolocation api call to be more performant by comparing current ip address with the last ip address added and if they match using that location data for current ip

--- app/controllers/parse_logs_controller.rb
+++ app/controllers/parse_logs_controller.rb
@@ -11,7 +11,7 @@ class ParseLogsController < ApplicationController
 
   def geolocation(ip_address)
     url = API_URL + ip_address.to_s
-    # API rate limited to 10, 000 requests an hour
+    # API rate limited to 15, 000 requests an hour
     response = open(url).read
     get_location_data = JSON.parse(response)
     return "#{get_location_data['region_name']}, #{get_location_data['country_name']}"
@@ -23,18 +23,24 @@ class ParseLogsController < ApplicationController
     last_log_time = Log.count > 0 ? Log.last.time : '0' # 0 needed if db is empty
     count = 0
     f.each do |line|
-      count += 1
-      if count == 1000
+      if count == 10000
         break
       else
         jsonline = JSON.parse(line.gsub("\u0000", ''))
         if (jsonline['protocol'] = 'ssh') && (jsonline['timestamp'] > last_log_time)
           if (jsonline['eventid'].include? 'cowrie.login')
-            p count
-            # String interpolation to return region and country separately
-            location = geolocation(jsonline['src_ip'])
-            region = location[0, location.index(',')]
-            country = location[location.index(', ')+2.. -1]
+            if Log.last.ip_address == jsonline['src_ip']
+              region = Log.last.region
+              country = Log.last.country
+              p region, country
+            else
+              count += 1
+              p count
+              # String interpolation to return region and country separately
+              location = geolocation(jsonline['src_ip'])
+              region = location[0, location.index(',')]
+              country = location[location.index(', ')+2.. -1]
+            end
             Log.create(time: jsonline['timestamp'].gsub("\u0000", ''),
                       status: jsonline['eventid'],
                       protocol: jsonline['protocol'],
