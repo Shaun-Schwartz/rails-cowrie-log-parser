e5a60012c9543243d671cb38a46bd51555f00629
Shaun Schwartz <me@shaunschwartz.com>
2018-01-13 13:50:22 -0800

    add check to make sure job has been added to db

--- app/controllers/parse_logs_controller.rb
+++ app/controllers/parse_logs_controller.rb
@@ -1,24 +1,27 @@
 class ParseLogsController < ApplicationController
   require 'generate_log.rb'
+  before_action :check_jobs
 
   def index
-    # parse_logs_db(FILE)
     @logs = Log.all.order(time: :desc).paginate(:page => params[:page], :per_page => 500)
   end
 
   def hour
-    # parse_logs_db(FILE)
     @logs = Log.where(time: (Time.now - 1.hour)..Time.now).order(time: :desc).paginate(:page => params[:page], :per_page => 500)
   end
 
   def day
-    # parse_logs_db(FILE)
     @logs = Log.where(time: (Time.now - 24.hours)..Time.now).order(time: :desc).paginate(:page => params[:page], :per_page => 500)
   end
 
   def week
-    # parse_logs_db(FILE)
     @logs = Log.where(time: (Time.now - 7.days)..Time.now).order(time: :desc).paginate(:page => params[:page], :per_page => 500)
+  end
+
+  def check_jobs
+    if Delayed::Job.count == 0
+      ParseLogsJob.set(wait: 5.minutes).perform_later
+    end
   end
 
 end
