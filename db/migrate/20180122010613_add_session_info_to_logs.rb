9ce650a33a1809d41d777c407e36f45d5ac76a45
Shaun Schwartz <me@shaunschwartz.com>
2018-01-21 17:26:39 -0800

    changle session_length column to float, add session length to _show_logs partial view

--- db/migrate/20180122010613_add_session_info_to_logs.rb
+++ db/migrate/20180122010613_add_session_info_to_logs.rb
@@ -1,6 +1,6 @@
 class AddSessionInfoToLogs < ActiveRecord::Migration[5.1]
   def change
     add_column :logs, :session_id, :string
-    add_column :logs, :session_length, :string
+    add_column :logs, :session_length, :float
   end
 end
